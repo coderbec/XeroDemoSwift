62e2b1737ca78d257d5d5c60c966a041f5fac645
coderbec <contact@becmartin.com.au>
2016-06-17 14:40:47 +1000

    Initial Commit

---
+++ XeroDemoSwift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  XeroDemoSwift
+//
+//  Created by Rebecca Martin on 17/06/2016.
+//  Copyright © 2016 Rebecca Martin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
